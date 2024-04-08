6883c82b5f1bdf9672e2df9f659c0d8d9fee6775
dacods <dacods.21@gmail.com>
2024-04-08 18:08:10 +0000

    Fixed bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,16 +7,16 @@
  * Return: 0
  */
 
-void swap(int *arr, int i, int j)
+void swap(int *arr, int i, int j, int size)
 {
 	int k;
 	int temp = arr[i];
 	arr[i] = arr[j];
 	arr[j] = temp;
-	for (k = 0; k < 10; k++)
+	for (k = 0; k < size; k++)
 	{
 		printf("%d", arr[k]);
-		if (k < 9)
+		if (k < size - 1)
 			printf(", ");
 	}
 	printf("\n");
@@ -29,17 +29,17 @@ void swap(int *arr, int i, int j)
  * Return: 0
  */
 
-void bubble_sort(int arr[], int n)
+void bubble_sort(int arr[], int size)
 {
 	int i, j;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < n - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (arr[j] > arr[j + 1])
 			{
-				swap(arr, j, j + 1);
+				swap(arr, j, j + 1, size);
 			}
 		}
 	}
--- sort.h
+++ sort.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-void swap(int *arr, int i, int j);
-void bubble_sort(int arr[], int n);
+void swap(int *arr, int i, int j, int size);
+void bubble_sort(int arr[], int size);
 
 #endif
